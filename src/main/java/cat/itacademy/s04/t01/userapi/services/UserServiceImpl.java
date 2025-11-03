213be6b196ef6a00cf5b1600efaa40c323c3266a
Adrià Lorente <adria810@gmail.com>
2025-11-03 10:22:41 +0100

    feat: implement unique email validation logic in UserServiceImpl

--- src/main/java/cat/itacademy/s04/t01/userapi/services/UserServiceImpl.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/services/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package cat.itacademy.s04.t01.userapi.services;
 
+import cat.itacademy.s04.t01.userapi.exceptions.EmailAlreadyExistsException;
 import cat.itacademy.s04.t01.userapi.exceptions.UserNotFoundException;
 import cat.itacademy.s04.t01.userapi.models.User;
 import cat.itacademy.s04.t01.userapi.repositories.UserRepository;
@@ -19,6 +20,10 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public User createUser(User userRequest) {
+        if (userRepository.existsByEmail(userRequest.email())) {
+            throw new EmailAlreadyExistsException(userRequest.email());
+        }
+
         UUID newId = UUID.randomUUID();
         User newUser = new User(newId, userRequest.name(), userRequest.email());
         return userRepository.save(newUser);
